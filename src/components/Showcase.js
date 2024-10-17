9ef15636cc69994688caf66757ec6918dff148dc
Janardhana Reddy M S <76699907+JANARDHANAREDDYMS@users.noreply.github.com>
2024-10-17 14:58:28 -0400

    Change the info and uni name

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -84,24 +84,23 @@ const Showcase = () =>{
                     right="1.5rem"
                     size="h3"
                     >
-                        <WhiteText>
-                        I'm a </WhiteText> <BlueText> Developer</BlueText>
+        
                     </Heading>
 
                     
 
                     <ParaText as="div" top="2rem" bottom="4rem">
                         <p style={{ paddingRight: "1rem" }}>
-                            <BlueText>I am Janardhana Reddy currently pursuing my undergrad in</BlueText>
+                            <BlueText>I am Janardhana Reddy currently pursuing my grad school in</BlueText>
                         </p>
                         <p style={{ paddingRight: "1rem" }}>
-                            <BlueText>Computer Science from Bangalore Institute of Technology.</BlueText>
+                            <BlueText>Computer Science from New York University.</BlueText>
                         </p>
                         <p style={{ paddingRight: "1rem" }}>
                             <BlueText>A passionate MERN developer, with interest in pursuit of</BlueText>
                         </p>
                         <p style={{ paddingRight: "13rem" }}>
-                            <BlueText>deep learningn and Computer Vision.</BlueText>
+                            <BlueText>deep learning and Computer Vision.</BlueText>
                         </p>
                     </ParaText>
 
